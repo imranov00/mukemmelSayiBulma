b9b21f7f4e294c92257edaf1469cec77f2b71fd7
İmranov <semtexpert@gmail.com>
2023-12-27 16:13:50 +0300

    version 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        System.out.println("Girilen Sayıların Mükemmel Bir Sayı olup olmadığını bulma programı.");
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Bir Sayı Giriniz:");
+        int sayi= input.nextInt();
+
+        int toplam =0 ;
+        for (int i = 1; i <= sayi/2; i++){
+
+            if (sayi % i == 0){
+                toplam +=i;
+            }
+        }
+        if (toplam == sayi){
+            System.out.println(sayi + "  Mükemmel Bir Sayıdır. ");
+        }else {
+            System.out.println(sayi + "  Mükemmel Sayı Değildir.");
+        }
     }
 }
